d51a3ec83ee5f4fc082f2e9c2c83b43530212499
Chen Weiguang <chen.weiguang@gmail.com>
2019-06-16 23:03:37 +0800

    Able to get all files

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use rusoto_s3::{
 use std::str::FromStr;
 use structopt::StructOpt;
 use tokio::prelude::{future, Future};
-use tokio::runtime;
+use tokio::runtime::{self, Runtime};
 
 type Error = Box<dyn std::error::Error>;
 
@@ -76,6 +76,73 @@ enum Subcommand {
     },
 }
 
+fn cp_action(
+    rt: &mut Runtime,
+    s3: &rusoto_s3::S3Client,
+    src_path: &S3Path,
+    matching_objs: impl Iterator<Item = rusoto_s3::Object>,
+) -> Result<(), Error> {
+    let get_obj_output_futs = matching_objs.map(move |matching_obj| {
+        let get_obj_req = GetObjectRequest {
+            bucket: src_path.bucket.clone(),
+            key: matching_obj.key.clone().unwrap(),
+            ..Default::default()
+        };
+
+        let rel_path = get_obj_req
+            .key
+            .trim_start_matches(&src_path.key)
+            .trim_start_matches('/')
+            .to_owned();
+
+        s3.get_object(get_obj_req)
+            .map(|fut| (rel_path, fut))
+            .map_err(|e| -> Error { e.into() })
+    });
+
+    let put_obj_output_futs = get_obj_output_futs.map(|get_obj_output_fut| {
+        // TODO and_then
+        get_obj_output_fut.map(|(key, get_obj_output)| {
+            println!(
+                "{}, content-length: {}",
+                key,
+                get_obj_output.content_length.unwrap()
+            );
+
+            get_obj_output
+
+            // // dst
+            // let put_obj_req = PutObjectRequest {
+            //     bucket: dst_path.bucket.clone(),
+            //     key: dst_path.key.clone(),
+            //     body: get_obj_output.body,
+            //     content_disposition: get_obj_output
+            //         .content_disposition,
+            //     content_language: get_obj_output
+            //         .content_language,
+            //     content_length: get_obj_output.content_length,
+            //     // content_md5: get_obj_output.content_md5,
+            //     content_type: get_obj_output.content_type,
+            //     metadata: get_obj_output.metadata,
+            //     ..Default::default()
+            // };
+
+            // dst_s3
+            //     .put_object(put_obj_req)
+            //     .map_err(|e| -> Error { e.into() })
+        })
+    });
+
+    for put_obj_output_fut in put_obj_output_futs {
+        rt.spawn(put_obj_output_fut.map(|_| ()).map_err(|e| {
+            eprintln!("Future error: {}", e);
+            ()
+        }));
+    }
+
+    Ok(())
+}
+
 fn main() -> Result<(), Error> {
     let args = Args::from_args();
     let provider = EnvironmentProvider::default();
@@ -95,80 +162,31 @@ fn main() -> Result<(), Error> {
             let src_path = S3Path::from_str(&src)?;
             let dst_path = S3Path::from_str(&dst)?;
 
-            let list_objs_req = ListObjectsV2Request {
-                bucket: src_path.bucket.clone(),
-                prefix: Some(src_path.key.clone()),
-                ..Default::default()
-            };
+            let mut rt = runtime::Builder::new().core_threads(4).build()?;
 
-            // Get all matching prefixes object
-            let matching_objs = s3
-                .list_objects_v2(list_objs_req)
-                .sync()?
-                .contents
-                .unwrap()
-                .into_iter();
+            // Get initial matching prefixes objects
+            let mut is_truncated = true;
+            let mut next_continuation_token = None;
 
-            // Get all object contents
-            let get_obj_output_futs = matching_objs.map(move |matching_obj| {
-                let get_obj_req = GetObjectRequest {
+            while is_truncated {
+                let list_objs_req = ListObjectsV2Request {
                     bucket: src_path.bucket.clone(),
-                    key: matching_obj.key.clone().unwrap(),
+                    prefix: Some(src_path.key.clone()),
+                    continuation_token: next_continuation_token,
                     ..Default::default()
                 };
 
-                let rel_path = get_obj_req
-                    .key
-                    .trim_start_matches(&src_path.key)
-                    .trim_start_matches('/')
-                    .to_owned();
+                let list_obj_output =
+                    s3.list_objects_v2(list_objs_req).sync()?;
+                let matching_objs =
+                    list_obj_output.contents.unwrap().into_iter();
 
-                s3.get_object(get_obj_req)
-                    .map(|fut| (rel_path, fut))
-                    .map_err(|e| -> Error { e.into() })
-            });
+                // Get all object contents
+                cp_action(&mut rt, &s3, &src_path, matching_objs)?;
 
-            let put_obj_output_futs =
-                get_obj_output_futs.map(|get_obj_output_fut| {
-                    // TODO and_then
-                    get_obj_output_fut.map(|(key, get_obj_output)| {
-                        println!(
-                            "{}, content-length: {}",
-                            key,
-                            get_obj_output.content_length.unwrap()
-                        );
-
-                        get_obj_output
-
-                        // // dst
-                        // let put_obj_req = PutObjectRequest {
-                        //     bucket: dst_path.bucket.clone(),
-                        //     key: dst_path.key.clone(),
-                        //     body: get_obj_output.body,
-                        //     content_disposition: get_obj_output
-                        //         .content_disposition,
-                        //     content_language: get_obj_output
-                        //         .content_language,
-                        //     content_length: get_obj_output.content_length,
-                        //     // content_md5: get_obj_output.content_md5,
-                        //     content_type: get_obj_output.content_type,
-                        //     metadata: get_obj_output.metadata,
-                        //     ..Default::default()
-                        // };
-
-                        // dst_s3
-                        //     .put_object(put_obj_req)
-                        //     .map_err(|e| -> Error { e.into() })
-                    })
-                });
-
-            let mut rt = runtime::Builder::new().core_threads(4).build()?;
-
-            for put_obj_output_fut in put_obj_output_futs {
-                rt.spawn(put_obj_output_fut.map(|_| ()).map_err(|e| {
-                    eprintln!("Future error: {}", e);
-                    ()
-                }));
+                is_truncated = list_obj_output.is_truncated.unwrap_or(false);
+                next_continuation_token =
+                    list_obj_output.next_continuation_token.clone();
             }
 
             rt.shutdown_on_idle().wait().unwrap();
