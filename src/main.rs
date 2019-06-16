e31689699921822109fafb4e231e28e665a1156b
Chen Weiguang <chen.weiguang@gmail.com>
2019-06-17 00:39:54 +0800

    Try to do blocking tasks

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@ use rusoto_credential::EnvironmentProvider;
 use rusoto_s3::{
     GetObjectRequest, ListObjectsV2Request, PutObjectRequest, S3Client, S3,
 };
-// use std::io::Read;
 use std::str::FromStr;
+use std::sync::{Arc, Mutex};
 use structopt::StructOpt;
 use tokio::prelude::{future, Future};
 use tokio::runtime::{self, Runtime};
+use tokio_timer::clock::Clock;
 
 type Error = Box<dyn std::error::Error>;
 
@@ -78,8 +79,10 @@ enum Subcommand {
 
 fn cp_action(
     rt: &mut Runtime,
-    s3: &rusoto_s3::S3Client,
+    s3: &S3Client,
+    dst_s3: Arc<Mutex<S3Client>>,
     src_path: &S3Path,
+    dst_path: Arc<Mutex<S3Path>>,
     matching_objs: impl Iterator<Item = rusoto_s3::Object>,
 ) -> Result<(), Error> {
     let get_obj_output_futs = matching_objs.map(move |matching_obj| {
@@ -89,56 +92,77 @@ fn cp_action(
             ..Default::default()
         };
 
-        let rel_path = get_obj_req
+        let rel_key = get_obj_req
             .key
             .trim_start_matches(&src_path.key)
             .trim_start_matches('/')
             .to_owned();
 
         s3.get_object(get_obj_req)
-            .map(|fut| (rel_path, fut))
+            .map(|fut| (rel_key, fut))
             .map_err(|e| -> Error { e.into() })
     });
 
     let put_obj_output_futs = get_obj_output_futs.map(|get_obj_output_fut| {
-        // TODO and_then
-        get_obj_output_fut.map(|(key, get_obj_output)| {
+        let dst_path = dst_path.clone();
+        let dst_s3 = dst_s3.clone();
+
+        get_obj_output_fut.and_then(move |(rel_key, get_obj_output)| {
+            let dst_path = dst_path.lock().unwrap();
+            let dst_bucket = dst_path.bucket.clone();
+            let dst_path_key = dst_path.key.trim_end_matches('/').clone();
+
+            let dst_key = format!("{}/{}", dst_path_key, rel_key,);
+
             println!(
-                "{}, content-length: {}",
-                key,
+                "{} -> {}, content-length: {}",
+                rel_key,
+                dst_key,
                 get_obj_output.content_length.unwrap()
             );
 
-            get_obj_output
+            // dst
+            let put_obj_req = PutObjectRequest {
+                bucket: dst_bucket,
+                key: dst_key,
+                body: get_obj_output.body,
+                content_disposition: get_obj_output.content_disposition,
+                content_language: get_obj_output.content_language,
+                content_length: get_obj_output.content_length,
+                content_type: get_obj_output.content_type,
+                metadata: get_obj_output.metadata,
+                ..Default::default()
+            };
 
-            // // dst
-            // let put_obj_req = PutObjectRequest {
-            //     bucket: dst_path.bucket.clone(),
-            //     key: dst_path.key.clone(),
-            //     body: get_obj_output.body,
-            //     content_disposition: get_obj_output
-            //         .content_disposition,
-            //     content_language: get_obj_output
-            //         .content_language,
-            //     content_length: get_obj_output.content_length,
-            //     // content_md5: get_obj_output.content_md5,
-            //     content_type: get_obj_output.content_type,
-            //     metadata: get_obj_output.metadata,
-            //     ..Default::default()
-            // };
-
-            // dst_s3
-            //     .put_object(put_obj_req)
-            //     .map_err(|e| -> Error { e.into() })
+            dst_s3
+                .lock()
+                .unwrap()
+                .put_object(put_obj_req)
+                .map_err(|e| -> Error { e.into() })
         })
     });
 
     for put_obj_output_fut in put_obj_output_futs {
-        rt.spawn(put_obj_output_fut.map(|_| ()).map_err(|e| {
-            eprintln!("Future error: {}", e);
-            ()
+        rt.spawn(future::lazy(move || {
+            put_obj_output_fut.map(|_| ()).map_err(|e| {
+                eprintln!("Future error: {}", e);
+                ()
+            })
         }));
     }
+
+    // for put_obj_output_fut in put_obj_output_futs {
+    //     rt.spawn(future::lazy(move || {
+    //         future::poll_fn(move || {
+    //             tokio_threadpool::blocking(|| {
+    //                 put_obj_output_fut.map(|_| ()).map_err(|e| {
+    //                     eprintln!("Future error: {}", e);
+    //                     ()
+    //                 })
+    //             })
+    //         })
+    //     }));
+    // }
 
     Ok(())
 }
@@ -151,18 +175,22 @@ fn main() -> Result<(), Error> {
     let s3 =
         S3Client::new_with(HttpClient::new()?, provider, Region::ApSoutheast1);
 
-    let dst_s3 = S3Client::new_with(
+    let dst_s3 = Arc::new(Mutex::new(S3Client::new_with(
         HttpClient::new()?,
         dst_provider,
         Region::ApSoutheast1,
-    );
+    )));
 
     match args.subcommand {
         Subcommand::Cp { src, dst } => {
             let src_path = S3Path::from_str(&src)?;
-            let dst_path = S3Path::from_str(&dst)?;
+            let dst_path = Arc::new(Mutex::new(S3Path::from_str(&dst)?));
 
-            let mut rt = runtime::Builder::new().core_threads(4).build()?;
+            let mut rt = runtime::Builder::new()
+                .blocking_threads(1)
+                .clock(Clock::system())
+                .core_threads(1)
+                .build()?;
 
             // Get initial matching prefixes objects
             let mut is_truncated = true;
@@ -181,8 +209,15 @@ fn main() -> Result<(), Error> {
                 let matching_objs =
                     list_obj_output.contents.unwrap().into_iter();
 
-                // Get all object contents
-                cp_action(&mut rt, &s3, &src_path, matching_objs)?;
+                // Perform the actual looping src to dst copy
+                cp_action(
+                    &mut rt,
+                    &s3,
+                    dst_s3.clone(),
+                    &src_path,
+                    dst_path.clone(),
+                    matching_objs,
+                )?;
 
                 is_truncated = list_obj_output.is_truncated.unwrap_or(false);
                 next_continuation_token =
